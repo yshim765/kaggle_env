9e548e766513661411da578a7684a157095aed4f
yshim765 <yshimizu765@gmail.com>
2021-04-01 22:14:29 +0900

    dockerコンテナを起動するシェルスクリプトを作成

---
+++ start_kaggle_docker.sh
@@ -0,0 +1,13 @@
+# kaggle の docker を起動するシェルスクリプト
+
+export NAME_IMAGE=kaggle-gpu
+
+# kaggle の docker コンテナが存在していた場合起動する。なければコンテナを建てる。
+if [ "$(docker ps -qa -f name="${NAME_IMAGE}")" ]; then
+    echo "Image ${NAME_IMAGE} already exist."
+    docker start ${NAME_IMAGE}
+else
+    docker run -itd  --gpus all  -p 8888:8888  -v $PWD/kaggle:/home/kaggle  -w /home/kaggle  --name ${NAME_IMAGE}  -h host  kaggle/python-gpu-build /bin/bash
+fi
+
+docker exec -it ${NAME_IMAGE} /bin/bash
