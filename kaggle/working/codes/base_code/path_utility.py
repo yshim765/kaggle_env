2708c7c7b25ef8a6c70f1f5f622abf1cdcf24c42
yshim765 <yshimizu765@gmail.com>
2021-05-27 09:36:54 +0000

    path はクラスで管理する方針へ変更

--- kaggle/working/codes/base_code/path_utility.py
+++ kaggle/working/codes/base_code/path_utility.py
@@ -1,31 +1,32 @@
+# %%
 import shutil
 from pathlib import Path
 
 
-def set_path(COMPETITION_NAME: str, EXPERIMENT_NAME: str, RUN_NAME: str, is_use_output_root: bool = False) -> dict:
-    PATH_DICT = {}
-    
-    PATH_DICT["ROOT"] = Path(".").resolve().parent
-    PATH_DICT["INPUT_ROOT"] = PATH_DICT["ROOT"] / "input"
-    PATH_DICT["RAW_DATA_DIR"] = PATH_DICT["INPUT_ROOT"] / COMPETITION_NAME
-    PATH_DICT["WORK_DIR"] = PATH_DICT["ROOT"] / "working"
-    PATH_DICT["OUTPUT_WORKDIR"] = PATH_DICT["WORK_DIR"] / "output"
-    PATH_DICT["PROC_DATA"] = PATH_DICT["ROOT"] / "processed_data"
+class KagglePath:
+    def __init__(self, COMPETITION_NAME: str, EXPERIMENT_NAME: str, RUN_NAME: str, is_use_output_root: bool = False) -> None:
+        tmp_current_dir = Path(".").resolve().parts
+        tmp_current_dir = tmp_current_dir[:tmp_current_dir.index("kaggle") + 1]
 
-    PATH_DICT["OUTPUT_WORKDIR"].mkdir(parents=True, exist_ok=True)
-    PATH_DICT["PROC_DATA"].mkdir(parents=True, exist_ok=True)
+        self.ROOT = Path().joinpath(*tmp_current_dir)
+        self.INPUT_ROOT = self.ROOT / "input"
+        self.RAW_DATA_DIR = self.INPUT_ROOT / COMPETITION_NAME
+        self.WORK_DIR = self.ROOT / "working"
+        self.OUTPUT_WORKDIR = self.WORK_DIR / "output"
+        self.PROC_DATA = self.ROOT / "processed_data"
 
-    # kaggle の code で回した時に output で表示する必要がない場合は
-    # 以下のoutputも使用する
-    if is_use_output_root:
-        PATH_DICT["OUTPUT_ROOT"] = PATH_DICT["ROOT"] / "output"
+        self.OUTPUT_WORKDIR.mkdir(parents=True, exist_ok=True)
+        self.PROC_DATA.mkdir(parents=True, exist_ok=True)
 
-        PATH_DICT["OUTPUT_ROOT"].mkdir(parents=True, exist_ok=True)
+        # kaggle の code で回した時に output で表示する必要がない場合は
+        # 以下のoutputも使用する
+        if is_use_output_root:
+            self.OUTPUT_ROOT = self.ROOT / "output"
 
-    PATH_DICT["SAVE_DIR"] = PATH_DICT["OUTPUT_WORKDIR"] / EXPERIMENT_NAME / RUN_NAME
+            self.OUTPUT_ROOT.mkdir(parents=True, exist_ok=True)
 
-    if PATH_DICT["SAVE_DIR"].exists():
-        shutil.rmtree(PATH_DICT["SAVE_DIR"])
-    PATH_DICT["SAVE_DIR"].mkdir(parents=True, exist_ok=True)
+        self.SAVE_DIR = self.OUTPUT_WORKDIR / EXPERIMENT_NAME / RUN_NAME
 
-    return PATH_DICT
+        if self.SAVE_DIR.exists():
+            shutil.rmtree(self.SAVE_DIR)
+        self.SAVE_DIR.mkdir(parents=True, exist_ok=True)
